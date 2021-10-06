011fb19e58b23df148999fce14a9b5ddec34b2ef
dovakeen118 <kegillis118@gmail.com>
2021-10-06 15:47:57 -0400

    update starter code

---
+++ spec/features/user_views_movies_list_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+feature "when the user navigates to the movies index page" do
+  scenario "an li of each movie is displayed" do
+    # 
+  end
+
+  scenario "they can click a link to navigate to the new movie form" do 
+    # 
+  end
+end
