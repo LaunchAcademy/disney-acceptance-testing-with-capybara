e5285666ff5f4373db70376b19b94b1622d0bbde
Nick Alberts <nwalberts5@gmail.com>
2022-06-28 17:24:38 -0400

    new changes boston 37 base

--- spec/features/user_adds_new_movie_spec.rb
+++ spec/features/user_adds_new_movie_spec.rb
@@ -3,28 +3,13 @@ require "spec_helper"
 feature "when a user visits the new movie form page" do
   context "if they fill out the form fields successfully" do 
     scenario "they will get redirected back to the index page and see their new movie" do
-      visit "/movies/new"
-
-      fill_in("Title", with: "Oliver and Company")
-      fill_in("Release Year", with: "1991")
-      fill_in("Runtime", with: "90 minutes")
-
-      click_button "Add New Movie"
-
-      expect(page).to have_current_path("/movies")
-      expect(page).to have_content("Oliver and Company")
+      
     end
   end
 
   context "when a user omits fields in the form" do
     scenario "they get errors describing which fields are blank" do
-      visit "/movies/new"
-
-      click_button "Add New Movie"
-
-      expect(page).to have_content("Title is blank")
-      expect(page).to have_content("Release Year is blank")
-      expect(page).to have_content("Runtime is blank")
+     
     end
   end
 end
--- spec/features/user_sees_movies_list_spec.rb
+++ spec/features/user_sees_movies_list_spec.rb
@@ -3,21 +3,10 @@ require "spec_helper"
 feature "when the user navigates to the movies index page" do
   scenario "an li of each movie is displayed" do
     # add to the CSV file before checking the page
-    CSV.open("movies_test.csv", "a") do |csv_file|
-      csv_file << ["The Great Mouse Detective", "1984", "80 minutes"]
-    end
 
-    visit "/movies" 
-
-    expect(page).to have_content("The Great Mouse Detective")
-    expect(page).to have_content("1984")
-    expect(page).to have_content("80 minutes")
   end
 
   scenario "they can click a link to navigate to the new movie form" do 
-      visit "/movies" 
-
-      click_link "Add a Movie"
-      expect(page).to have_current_path("/movies/new")
+     
   end
 end
